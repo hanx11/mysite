9093bd89e596a0074fb48790e1f297a44c2962d3
hanx11 <hanx11@163.com>
2016-01-12 11:49:50 +0800

    add project file

---
+++ mysite/views.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+
+from django.http import HttpResponse
+import hashlib
+
+def handleRequest(request):
+    if request.method == 'GET':
+        response = HttpResponse(checkSignature(request), content_type="text/plain")
+        return response
+    else:
+    	return None
+
+def checkSignature(request):
+	TOKEN = "hanfeng"
+	signature = request.GET.get("signature", None)  
+	timestamp = request.GET.get("timestamp", None)  
+	nonce = request.GET.get("nonce", None)  
+	echoStr = request.GET.get("echostr", None)  
+
+	token = TOKEN  
+	tmpList = [token,timestamp,nonce]  
+	tmpList.sort()  
+	tmpstr = "%s%s%s" % tuple(tmpList)  
+	tmpstr = hashlib.sha1(tmpstr).hexdigest()  
+	if tmpstr == signature:
+		return echoStr  
+	else:
+		return None 
+
