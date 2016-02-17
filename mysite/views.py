cd9377f86650a90130b07859fe3493af222203f0
hanx11 <hanx11@163.com>
2016-02-17 11:50:47 +0800

    modify mysite/views.py

--- mysite/views.py
+++ mysite/views.py
@@ -20,51 +20,13 @@ YOUDAO_DOC_TYPE = "xml"
 
 def handleRequest(request):
     if request.method == 'GET':
-        checkSignature()
-    elif request.method == 'POST':
-        msg = parse_msg(request)           #进行XML解析
-        toUserName = msg['ToUserName']
-        fromUserName = msg['FromUserName']
-        createTime = msg['CreateTime']
-        msgType = msg['MsgType']
-        content = msg['Content']   #获得用户所输入的内容
-        msgId = msg['MsgId']
-        return render(request, 'reply_text.xml',
-                      {'toUserName': fromUserName,
-                       'fromUserName': toUserName,
-                       'createTime': time.time(),
-                       'msgType': msgType,
-                       'content': content,
-                       },
-                       content_type = 'application/xml'
-        )
+        response = HttpResponse(checkSignature(request), content_type="text/plain")
+        return response
     else:
         return None
 
 def checkSignature(request):
 	TOKEN = "xiaoxiao"
-    #得到GET内容
-    signature = request.GET.get('signature', None)
-    timestamp = request.GET.get('timestamp', None)
-    nonce = request.GET.get('nonce', None)
-    echostr = request.GET.get('echostr', None)
-    #自己的token
-    token = TOKEN   #这里改写你在微信公众平台里输入的token
-    #字典序排序
-    tmpList = [token, timestamp, nonce]
-    tmpList.sort()
-    tmpstr = '%s%s%s' % tuple(tmpList)
-    tmpstr = tmpstr.encode('utf-8')
-    #sha1加密算法
-    tmpstr = hashlib.sha1(tmpstr).hexdigest()
-        
-    #如果是来自微信的请求，则回复echostr
-    if tmpstr == signature:
-        return render(request, 'get.html', {'str': echostr}, content_type='text/plain')
-
-
-
-
 	signature = request.GET.get("signature", None)  
 	timestamp = request.GET.get("timestamp", None)  
 	nonce = request.GET.get("nonce", None)  
