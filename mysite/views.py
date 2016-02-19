f5240d11b5fc0044a1f1f2b528ffe0ae4be40bb5
hanx11 <hanx11@163.com>
2016-02-19 10:49:25 +0800

    modify mysite/views.py

--- mysite/views.py
+++ mysite/views.py
@@ -133,8 +133,8 @@ class YouDaoInterfaceView(View):
     def post(self, request):
         msg = parse_msg(request)      #进行xml解析
         # pdb.set_trace()
-        queryStr = msg.get('Content', 'You have input nothing~')
-        # print(queryStr)
+        queryStr = msg['Content']
+        print(queryStr)
         # print(msg.get('Content').encode('utf-8'))
         query_data = {'keyfrom':'hanfeng', 'key':'692856525', 'type':'data', 'doctype':'json', 'version':'1.1', 'q':queryStr}
         response = requests.get("http://fanyi.youdao.com/openapi.do", params=query_data)
