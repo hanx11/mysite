89ab9d6dff1ef21c2b97a853c6c302de28e1571b
hanx11 <hanx11@163.com>
2016-02-22 13:56:27 +0800

    modify views.py

--- mysite/views.py
+++ mysite/views.py
@@ -69,106 +69,106 @@ def weiXinInterfaceView(request):
         # msg = parse_msg(request)
         return HttpResponse("hello", content_type="text/plain")
 
-def parseYouDaoResponse(rep):
-    replyContent = ''
-    # pdb.set_trace()
-    result = json.loads(rep.text, encoding='utf-8')
-    errorCode = result.get('errorCode')
-    print(errorCode)
-    # 错误码检查
-    if errorCode == '20':
-        replyContent = "Too long to translate\n"
-        return replyContent
-    elif errorCode == '30':
-        replyContent = "Can not be able to translate with effect\n"
-        return replyContent
-    elif errorCode == '40':
-        replyContent = "Can not be able to support this language\n"
-        return replyContent
-    elif errorCode == '50':
-        replyContent = "Invalid key\n"
-        return replyContent
-    elif errorCode == '0':
-        queryData = result.get('query')
-        print(queryData)
-        translation = result.get('translation', '123')[0].encode('utf-8')
-        basicPhonetic = result.get('basic').get('phonetic')
-        basicExplains = result.get('basic').get('explains')[0]
-        # print(translation)
-        replyContent = translation
-        return replyContent
+# def parseYouDaoResponse(rep):
+#     replyContent = ''
+#     # pdb.set_trace()
+#     result = json.loads(rep.text, encoding='utf-8')
+#     errorCode = result.get('errorCode')
+#     print(errorCode)
+#     # 错误码检查
+#     if errorCode == '20':
+#         replyContent = "Too long to translate\n"
+#         return replyContent
+#     elif errorCode == '30':
+#         replyContent = "Can not be able to translate with effect\n"
+#         return replyContent
+#     elif errorCode == '40':
+#         replyContent = "Can not be able to support this language\n"
+#         return replyContent
+#     elif errorCode == '50':
+#         replyContent = "Invalid key\n"
+#         return replyContent
+#     elif errorCode == '0':
+#         queryData = result.get('query')
+#         print(queryData)
+#         translation = result.get('translation', '123')[0].encode('utf-8')
+#         basicPhonetic = result.get('basic').get('phonetic')
+#         basicExplains = result.get('basic').get('explains')[0]
+#         # print(translation)
+#         replyContent = translation
+#         return replyContent
 
-def getReplyXml(msg,replyContent):
-    extTpl = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[%s]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>";
-    extTpl = extTpl % (msg['FromUserName'],msg['ToUserName'],str(int(time.time())),'text',replyContent)
-    return extTpl
+# def getReplyXml(msg,replyContent):
+#     extTpl = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[%s]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>";
+#     extTpl = extTpl % (msg['FromUserName'],msg['ToUserName'],str(int(time.time())),'text',replyContent)
+#     return extTpl
 
 
-class WeixinInterfaceView(View):
-    def get(self, request):
-        # 得到GET内容
-        signature = request.GET.get('signature', None)
-        timestamp = request.GET.get('timestamp', None)
-        nonce = request.GET.get('nonce', None)
-        echostr = request.GET.get('echostr', None)
-        #自己的token
-        token = 'xiaoxiao'   #这里改写你在微信公众平台里输入的token
-        #字典序排序
-        tmpList = [token, timestamp, nonce]
-        tmpList.sort()
-        tmpstr = '%s%s%s' % tuple(tmpList)
-        tmpstr = tmpstr.encode('utf-8')
-        #sha1加密算法
-        tmpstr = hashlib.sha1(tmpstr).hexdigest()
+# class WeixinInterfaceView(View):
+#     def get(self, request):
+#         # 得到GET内容
+#         signature = request.GET.get('signature', None)
+#         timestamp = request.GET.get('timestamp', None)
+#         nonce = request.GET.get('nonce', None)
+#         echostr = request.GET.get('echostr', None)
+#         #自己的token
+#         token = 'xiaoxiao'   #这里改写你在微信公众平台里输入的token
+#         #字典序排序
+#         tmpList = [token, timestamp, nonce]
+#         tmpList.sort()
+#         tmpstr = '%s%s%s' % tuple(tmpList)
+#         tmpstr = tmpstr.encode('utf-8')
+#         #sha1加密算法
+#         tmpstr = hashlib.sha1(tmpstr).hexdigest()
         
-        #如果是来自微信的请求，则回复echostr
-        if tmpstr == signature:
-            return render(request, 'get.html', {'str': echostr},
-                          content_type='text/plain')
+#         #如果是来自微信的请求，则回复echostr
+#         if tmpstr == signature:
+#             return render(request, 'get.html', {'str': echostr},
+#                           content_type='text/plain')
 
-    def post(self, request):
-        pdb.set_trace()
-        msg = parse_msg(request)           #进行XML解析
-        toUserName = msg['ToUserName']
-        fromUserName = msg['FromUserName']
-        createTime = msg['CreateTime']
-        msgType = msg['MsgType']
-        content = msg['Content']   #获得用户所输入的内容
-        msgId = msg['MsgId']
-        return render(request, 'reply_text.xml',
-                      {'toUserName': fromUserName,
-                       'fromUserName': toUserName,
-                       'createTime': time.time(),
-                       'msgType': msgType,
-                       'content': content,
-                       },
-                       content_type = 'application/xml'
-        )
+#     def post(self, request):
+#         pdb.set_trace()
+#         msg = parse_msg(request)           #进行XML解析
+#         toUserName = msg['ToUserName']
+#         fromUserName = msg['FromUserName']
+#         createTime = msg['CreateTime']
+#         msgType = msg['MsgType']
+#         content = msg['Content']   #获得用户所输入的内容
+#         msgId = msg['MsgId']
+#         return render(request, 'reply_text.xml',
+#                       {'toUserName': fromUserName,
+#                        'fromUserName': toUserName,
+#                        'createTime': time.time(),
+#                        'msgType': msgType,
+#                        'content': content,
+#                        },
+#                        content_type = 'application/xml'
+#         )
 
 
-class YouDaoInterfaceView(View):
-    def get(self, request):
-        return HttpResponse(checkSignature(request), content_type="text/plain")
+# class YouDaoInterfaceView(View):
+#     def get(self, request):
+#         return HttpResponse(checkSignature(request), content_type="text/plain")
 
-    def post(self, request):
-        msg = parse_msg(request)      #进行xml解析
-        pdb.set_trace()
-        queryStr = msg['Content']
-        query_data = {'keyfrom':'hanfeng', 'key':'692856525', 'type':'data', 'doctype':'json', 'version':'1.1', 'q':queryStr}
-        response = requests.get("http://fanyi.youdao.com/openapi.do", params=query_data)
-        replyContent = parseYouDaoResponse(response)
-        toUserName = msg['ToUserName']
-        fromUserName = msg['FromUserName']
-        createTime = msg['CreateTime']
-        msgType = msg['MsgType']
-        msgId = msg['MsgId']
-        return render(request, 'reply_text.xml',
-                      {'toUserName': fromUserName,
-                       'fromUserName': toUserName,
-                       'createTime': time.time(),
-                       'msgType': msgType,
-                       'content': replyContent,
-                       },
-                       content_type = 'application/xml'
-        )
+#     def post(self, request):
+#         msg = parse_msg(request)      #进行xml解析
+#         pdb.set_trace()
+#         queryStr = msg['Content']
+#         query_data = {'keyfrom':'hanfeng', 'key':'692856525', 'type':'data', 'doctype':'json', 'version':'1.1', 'q':queryStr}
+#         response = requests.get("http://fanyi.youdao.com/openapi.do", params=query_data)
+#         replyContent = parseYouDaoResponse(response)
+#         toUserName = msg['ToUserName']
+#         fromUserName = msg['FromUserName']
+#         createTime = msg['CreateTime']
+#         msgType = msg['MsgType']
+#         msgId = msg['MsgId']
+#         return render(request, 'reply_text.xml',
+#                       {'toUserName': fromUserName,
+#                        'fromUserName': toUserName,
+#                        'createTime': time.time(),
+#                        'msgType': msgType,
+#                        'content': replyContent,
+#                        },
+#                        content_type = 'application/xml'
+#         )
 
