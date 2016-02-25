4555895f49e8bb09491b681cdfea4297adffbb12
hanx11 <hanx11@163.com>
2016-02-25 11:37:07 +0800

    modify mysite/views.py

--- mysite/views.py
+++ mysite/views.py
@@ -45,6 +45,7 @@ def checkSignature(request):
 def parse_msg(request):
     # 解析来自微信的请求，request用于传递请求信息
     recvmsg = request.body
+    print(request.body)
     root = ET.fromstring(recvmsg)
     msg = {}
     for child in root:
