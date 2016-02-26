2b6ff13b7fc3d3a281e75fe317aa77e19f4b2baf
hanx11 <hanx11@163.com>
2016-02-26 16:00:36 +0800

    modify mysite/views.py

--- mysite/views.py
+++ mysite/views.py
@@ -86,7 +86,7 @@ def parseYouDaoResponse(response):
         explains = result.get('basic').get('explains')
         explain = ''
         for exp in explains:
-            explain = explain + ',' + exp
+            explain = explain + '\n\t' + exp
         # basicExplains = result.get('basic').get('explains')[0]
         replyContent = "%s\n--有道翻译--\n翻译:%s\n解释:%s\n" % (queryData, translation, explain)  
         return replyContent
