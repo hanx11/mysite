2f0752daebc4ab2d3a989e9e0621dc632358e4ed
hanx11 <hanx11@163.com>
2016-09-18 17:18:32 +0800

    modify mysite/settings-release.py for release

--- mysite/settings-release.py
+++ mysite/settings-release.py
@@ -56,6 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            '/root/workspace/mysite/templates',
             '/root/workspace/static/',
             '/root/workspace/mysite/mysite/templates',
         ],
